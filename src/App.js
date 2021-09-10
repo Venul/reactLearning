0a8bbaa8261bcc943added5394997d0a1d40e1e8
Lunev Sergey <s.lunev88@gmail.com>
2021-09-11 01:43:49 +0300

    3.1 Create State

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
+import React, { Component } from 'react';
 import './App.css';
 import Car from './Car/Car';
 
-function App() {
-  return (
-    <div>
-      <Car name={'Audi'} year={2018}/>
-      <Car name="Ford" year={2015}/>
-   </div>
-  );
+class App extends Component {
+
+  state = {
+    cars: [
+      {name: 'Audi', year: 2018},
+      {name: 'Ford', year: 2015},
+      {name: 'Nissan', year: 2016}
+    ],
+    pageTitle: 'Hell world'
+  }
+
+  render() {
+    const cars = this.state.cars;
+
+    return (
+      <div>
+        <h1>{this.state.pageTitle}</h1>
+        <Car name={cars[0].name} year={cars[0].year}/>
+        <Car name={cars[1].name} year={cars[1].year}/>
+    </div>
+    );
+  }
 }
 
 export default App;
